615579d02071e005eb04fa167017a48d5a6f69ac
furubarug <83177512+furubarug@users.noreply.github.com>
2021-04-26 03:19:13 +0900

    Refactor

--- fanbox-downloader.js
+++ fanbox-downloader.js
@@ -1,53 +1,38 @@
-var dlList = new Object();
-dlList.items = new Array();
+let dlList = {};
+dlList.items = [];
 dlList.postCount = 0;
 dlList.fileCount = 0;
-var isIgnoreFree = false;
+let isIgnoreFree = false;
 
 // 投稿の情報を個別に取得しない（基本true）
-var isEco = true;
+let isEco = true;
 
 function main() {
-    if (window.location.origin == "https://downloads.fanbox.cc") {
+    if (window.location.origin === "https://downloads.fanbox.cc") {
         document.body.innerHTML = "";
-        var tb = document.createElement("input");
+        let tb = document.createElement("input");
         tb.type = "text";
-        var bt = document.createElement("input");
+        let bt = document.createElement("input");
         bt.type = "button";
         bt.value = "ok";
         document.body.appendChild(tb);
         document.body.appendChild(bt);
-        bt.onclick = function(){
+        bt.onclick = function () {
             JSON.parse(tb.value).items.forEach(dl => {
                 createLink(dl.url, dl.filename);
             });
             startDownload();
         };
-        
-    } else if (window.location.origin == "https://www.fanbox.cc") {
+
+    } else if (window.location.origin === "https://www.fanbox.cc") {
         if (window.location.href.match(/fanbox.cc\/@.*\/posts\/(\d*)/) == null) {
-            dlList.items = new Array();
-            var id = window.location.href.match(/fanbox.cc\/@(.*)/)[1];
-
-            isIgnoreFree = confirm("無料コンテンツを省く？");
-
-            var limit = prompt("取得制限数(最大300)を入力 キャンセルで最後まで取得");
-            if (limit == null){
-                var count=1;
-                var nextUrl="https://api.fanbox.cc/post.listCreator?creatorId=" + id + "&limit=100";
-                for(;nextUrl!=null;count++){
-                    console.log(count+"回目");
-                    var nextUrl = addByPostListUrl(nextUrl, isEco);
-                }
-            }else{
-                // limit=300が限界らしい？
-                var nextUrl = addByPostListUrl("https://api.fanbox.cc/post.listCreator?creatorId=" + id + "&limit=" + limit, isEco);
-            }
+            const id = window.location.href.match(/fanbox.cc\/@(.*)/)[1];
+            getItemsById(id);
         } else {
-            var id = window.location.href.match(/fanbox.cc\/@.*\/posts\/(\d*)/)[1];
+            const id = window.location.href.match(/fanbox.cc\/@.*\/posts\/(\d*)/)[1];
             addByPostInfo(getPostInfoById(id));
         }
-        var json=JSON.stringify(dlList);
+        const json = JSON.stringify(dlList);
         navigator.clipboard.writeText(json);
         console.log(json);
         alert("jsonをコピーしました。downloads.fanbox.ccで実行して貼り付けてね");
@@ -58,32 +43,50 @@ function main() {
 
 // 投稿リストURLからURLリストに追加
 function addByPostListUrl(url, eco) {
-    var postList = JSON.parse(fetchUrl(url));
-    var items = postList.body.items;
+    const postList = JSON.parse(fetchUrl(url));
+    const items = postList.body.items;
 
-    console.log("投稿の数:"+items.length);
-    for (var i = 0; i < items.length; i++) {
+    console.log("投稿の数:" + items.length);
+    for (let i = 0; i < items.length; i++) {
         dlList.postCount++;
         // ecoがtrueならpostInfoを個別に取得しない
-        if(eco==true){
+        if (eco === true) {
             console.log(items[i]);
             addByPostInfo(items[i]);
-        }else{
+        } else {
             addByPostInfo(getPostInfoById(items[i].id));
         }
-        
-    }
 
+    }
     return postList.body.nextUrl;
 }
 
 // HTTP GETするおまじない
-function fetchUrl(url){
-    var request = new XMLHttpRequest();
+function fetchUrl(url) {
+    const request = new XMLHttpRequest();
     request.open('GET', url, false);
     request.withCredentials = true;
     request.send(null);
     return request.responseText;
+}
+
+// 投稿IDからitemsを得る
+function getItemsById(postId) {
+    dlList.items = [];
+    isIgnoreFree = confirm("無料コンテンツを省く？");
+
+    let limit = prompt("取得制限数(最大300)を入力 キャンセルで最後まで取得");
+    if (limit == null) {
+        let count = 1, nextUrl;
+        nextUrl = "https://api.fanbox.cc/post.listCreator?creatorId=" + postId + "&limit=100";
+        for (; nextUrl != null; count++) {
+            console.log(count + "回目");
+            nextUrl = addByPostListUrl(nextUrl, isEco);
+        }
+    } else {
+        // limit=300が限界らしい？
+        addByPostListUrl("https://api.fanbox.cc/post.listCreator?creatorId=" + postId + "&limit=" + limit, isEco);
+    }
 }
 
 // 投稿IDからpostInfoを得る
@@ -93,39 +96,39 @@ function getPostInfoById(postId) {
 
 // postInfoオブジェクトからURLリストに追加する
 function addByPostInfo(postInfo) {
-    var title = postInfo.title;
+    const title = postInfo.title;
     // クリエイター名より日付入ってたほうがうれしいのでかえた（きまぐれ）
-    
-    var date = postInfo.publishedDatetime;
-    if(isIgnoreFree && (postInfo.feeRequired==0)){
+
+    const date = postInfo.publishedDatetime;
+    if (isIgnoreFree && (postInfo.feeRequired === 0)) {
         return;
     }
 
-    if(postInfo.body==null){
+    if (postInfo.body == null) {
         console.log("取得できませんでした(支援がたりない？)\n" + "feeRequired: " + postInfo.feeRequired + "@" + postInfo.id);
         return;
     }
 
-    if (postInfo.type == "image") {
-        var images = postInfo.body.images;
-        for (var i = 0; i < images.length; i++) {
-            addUrl(images[i].originalUrl, date + " " + title + " " + (i+1) + "." + images[i].extension);
+    if (postInfo.type === "image") {
+        const images = postInfo.body.images;
+        for (let i = 0; i < images.length; i++) {
+            addUrl(images[i].originalUrl, date + " " + title + " " + (i + 1) + "." + images[i].extension);
         }
-    } else if (postInfo.type == "file") {
-        var files = postInfo.body.files;
-        for (var i = 0; i < files.length; i++) {
+    } else if (postInfo.type === "file") {
+        const files = postInfo.body.files;
+        for (let i = 0; i < files.length; i++) {
             addUrl(files[i].url, date + " " + title + " " + files[i].name + "." + files[i].extension);
         }
-    } else if (postInfo.type == "article") {
-        var imageMap = postInfo.body.imageMap;
-        var imageMapKeys = Object.keys(imageMap);
-        for (var i = 0; i < imageMapKeys.length; i++) {
-            addUrl(imageMap[imageMapKeys[i]].originalUrl, date + " " + title + " " + (i+1) + "." + imageMap[imageMapKeys[i]].extension);
+    } else if (postInfo.type === "article") {
+        const imageMap = postInfo.body.imageMap;
+        const imageMapKeys = Object.keys(imageMap);
+        for (let i = 0; i < imageMapKeys.length; i++) {
+            addUrl(imageMap[imageMapKeys[i]].originalUrl, date + " " + title + " " + (i + 1) + "." + imageMap[imageMapKeys[i]].extension);
         }
 
-        var fileMap = postInfo.body.fileMap;
-        var fileMapKeys = Object.keys(fileMap);
-        for (var i = 0; i < fileMapKeys.length; i++) {
+        const fileMap = postInfo.body.fileMap;
+        const fileMapKeys = Object.keys(fileMap);
+        for (let i = 0; i < fileMapKeys.length; i++) {
             addUrl(fileMap[fileMapKeys[i]].url, date + " " + title + " " + fileMap[fileMapKeys[i]].name + "." + fileMap[fileMapKeys[i]].extension);
         }
     } else {
@@ -135,7 +138,7 @@ function addByPostInfo(postInfo) {
 
 // URLリストに追加
 function addUrl(url, filename) {
-    var dl = new Object();
+    const dl = {};
     dl.url = url;
     dl.filename = filename;
 
@@ -145,7 +148,7 @@ function addUrl(url, filename) {
 
 // ダウンロードリンクを作成
 function createLink(url, filename) {
-    var a = document.createElement("a");
+    const a = document.createElement("a");
     document.body.appendChild(a);
     a.href = url;
     a.download = filename;
@@ -153,9 +156,9 @@ function createLink(url, filename) {
 
 // ダウンロードリンクをクリックする
 function startDownload() {
-    var links = document.querySelectorAll("a");
-    var idx = 0;
-    var interval = setInterval(function () {
+    const links = document.querySelectorAll("a");
+    let idx = 0;
+    const interval = setInterval(function () {
         links[idx].click();
         idx++;
         if (idx >= links.length) clearInterval(interval);
