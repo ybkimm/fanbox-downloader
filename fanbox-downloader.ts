223c9f32df03ca54b1360a91d965c09fa44cf3a5
furubarug <83177512+furubarug@users.noreply.github.com>
2022-02-03 17:25:11 +0900

    Refactor

--- fanbox-downloader.ts
+++ fanbox-downloader.ts
@@ -178,16 +178,16 @@ async function getItemsById(downloadManage: DownloadManage) {
 function addByPostListUrl(downloadManage: DownloadManage, url: string): string | null {
 	const postList =
 		DownloadManage.utils.httpGetAs<{ body: { items: PostInfo[]; nextUrl: string | null } }>(url);
-	const items = postList.body.items;
-
-	console.log(`投稿の数:${items.length}`);
-	for (let i = 0; i < items.length && downloadManage.isLimitValid(); i++) {
-		if (downloadManage.isEco) {
-			console.log(items[i]);
-			addByPostInfo(downloadManage, items[i]);
-		} else {
-			addByPostInfo(downloadManage, getPostInfoById(items[i].id));
-		}
+	console.log(`投稿の数:${postList.body.items.length}`);
+	for (const item of postList.body.items) {
+		if (downloadManage.isLimitValid()) {
+			if (downloadManage.isEco) {
+				console.log(item);
+				addByPostInfo(downloadManage, item);
+			} else {
+				addByPostInfo(downloadManage, getPostInfoById(item.id));
+			}
+		} else break;
 	}
 	return postList.body.nextUrl;
 }
